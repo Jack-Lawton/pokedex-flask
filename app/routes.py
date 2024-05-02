c6f93938e2ea92eea81fb7934d81a7596a5e8aeb
Jack Lawton <jlawton@aiimi.com>
2024-05-02 14:35:57 +0100

    Load pokedex on app launch, not every request - and add type filtered page

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,25 @@ import json
 import ast
 import os
 
+f = open("api-cache.txt", "r")
+pokedex_dict_raw = ast.literal_eval(f.read())
+
+pokedex_dict = {}
+
+for i in range(1000):
+    if i in pokedex_dict_raw:
+        pokedex_dict[i] = pokedex_dict_raw[i]
+
 @app.route('/')
 @app.route('/index')
 def index():
-    f = open("api-cache.txt", "r")
-    pokedex_dict = ast.literal_eval(f.read())
     return render_template('index.html', pokedex = pokedex_dict, entries = len(pokedex_dict)+1)
+
+@app.route('/type/<f>')
+def type_filter(f):
+    type_dict = {}
+    for key, value in pokedex_dict.items():
+        if f in value["type"]:
+            type_dict[len(type_dict) + 1] = value
+    return render_template('index.html', pokedex = type_dict, entries = len(type_dict)+1)
+
